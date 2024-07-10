a0aa7ba55e05f669f146f6cd36ea65a27a2e83ac
Mikhail <mikhailyarkov2000@gmail.com>
2024-07-10 11:45:52 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [score, setScore] = useState('')
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
-    // const score = urlParams.get('score');
-
+    const score = urlParams.get('score');
+    setScore(score)
   }, [])
   return (
     <div className="App">
-      Score: 
+      Score: {score}
     </div>
   );
 }
